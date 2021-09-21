e63ac6d2c858e1057b4eca60d9d6755b7e05ccf7
Param Choudhary <choudharyparam99@gmail.com>
2021-09-21 23:44:00 +0530

    my second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+
+import './App.css';
+import Users from './components/Users.js';
+
+
+
+
+function App() {
+ 
+  return (
+
+<div className= "App">
+<Users/>
+
+</div>
+
+  );
+}
+
+export default App;
